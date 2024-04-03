ce81bb7b8079f65f64b91a7a877dd4c16dfd7e70
2sjdeveloper <2sjdeveloper@gmail.com>
2024-04-03 19:01:47 +0900

    240403 - 자재검사대기 페이지

--- theCaffeine/src/main/java/com/theCaffeine/mes/qlty/service/impl/QltyServiceImpl.java
+++ theCaffeine/src/main/java/com/theCaffeine/mes/qlty/service/impl/QltyServiceImpl.java
@@ -1,5 +1,23 @@
 package com.theCaffeine.mes.qlty.service.impl;
 
-public class QltyServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.theCaffeine.mes.qlty.mapper.QltyMapper;
+import com.theCaffeine.mes.qlty.model.QltyVO;
+import com.theCaffeine.mes.qlty.service.QltyService;
+
+@Service
+public class QltyServiceImpl implements QltyService{
+	
+	@Autowired
+	QltyMapper qltyMapper;
+	
+	@Override
+	public List<QltyVO> getMtrlCheckList(QltyVO vo) {
+		return qltyMapper.getMtrlCheckList(vo);
+	}
 
 }
